a80f92a76fe49ee6019a6aaa6efb5c89d1b21b0e
jedi10 <kyle10_dev@hotmail.fr>
2020-07-30 06:33:33 +0200

    Unit Test PersonDao for convertJsonRootDataToJava: test Failed
    convertJsonRootDataToJava is a tool method used by Dao to obtain data. Test failed because objects returned are linkedhashMap instead of Person Object.

--- alerts/src/test/java/com/safetynet/alerts/dao/PersonDaoImplTest.java
+++ alerts/src/test/java/com/safetynet/alerts/dao/PersonDaoImplTest.java
@@ -12,6 +12,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @Slf4j
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -19,33 +21,35 @@ import java.util.Map;
 class PersonDaoImplTest {
 
     private static Map<String, List<Person>> wrapperList = new HashMap<>();
-    private static List<Person> persons = new ArrayList<>();
-
-    static {
-        Person person = new Person("julia", "werner", "rue du colysée", "Rome", 45, "06-12-23-34-45", "wermer@mail.it");
-        Person person2 = new Person("judy", "holmes", "rue de la pensee", "Londre", 89, "06-25-74-90-12", "holmes@mail.en");
-        Person personCreated = new Person("jack", "mortimer", "rue du stade", "Rome", 45, "06-25-50-90-12", "mortimer@mail.it");
-        //personList = List.of(person, person2);//immutable
-        persons.add(person);
-        persons.add(person2);
-        wrapperList.put("persons", persons);
-    }
-
-    //String staticJsonData = "{\"persons\": [  { \"firstName\":\"John\", \"lastName\":\"Boyd\", \"address\":\"1509 Culver St\", \"city\":\"Culver\", \"zip\":\"97451\", \"phone\":\"841-874-6512\", \"email\":\"jaboyd@email.com\" },{ \"firstName\":\"Jacob\", \"lastName\":\"Boyd\", \"address\":\"1509 Culver St\", \"city\":\"Culver\", \"zip\":\"97451\", \"phone\":\"841-874-6513\", \"email\":\"drk@email.com\" }]}";
-
+    private static List<Person> personsGiven = new ArrayList<>();
 
     @Autowired
     private IPersonDAO iPersonDAO;
 
+    static {
+        //GIVEN
+        Person person = new Person("julia", "werner", "rue du colysée", "Rome", 45, "06-12-23-34-45", "wermer@mail.it");
+        Person person2 = new Person("judy", "holmes", "rue de la pensee", "Londre", 89, "06-25-74-90-12", "holmes@mail.en");
+        Person personCreated = new Person("jack", "mortimer", "rue du stade", "Rome", 45, "06-25-50-90-12", "mortimer@mail.it");
+        //personList = List.of(person, person2);//immutable
+        personsGiven.add(person);
+        personsGiven.add(person2);
+        wrapperList.put("persons", personsGiven);
+    }
+
+    //String staticJsonData = "{\"persons\": [  { \"firstName\":\"John\", \"lastName\":\"Boyd\", \"address\":\"1509 Culver St\", \"city\":\"Culver\", \"zip\":\"97451\", \"phone\":\"841-874-6512\", \"email\":\"jaboyd@email.com\" },{ \"firstName\":\"Jacob\", \"lastName\":\"Boyd\", \"address\":\"1509 Culver St\", \"city\":\"Culver\", \"zip\":\"97451\", \"phone\":\"841-874-6513\", \"email\":\"drk@email.com\" }]}";
+
     @BeforeAll
     void setUp(){
+        //GIVEN
         String personsJson = Jackson.convertJavaToJson(wrapperList);
         this.iPersonDAO.getRootFile().setBytes(personsJson.getBytes());
+        /**
         iPersonDAO.setPersonList(Jackson.convertJsonRootDataToJava(
                 this.iPersonDAO.getRootFile().getBytes(),
                 "persons",
                 Person.class)
-        );
+        );**/
         //log.warn(persons.toString());
     }
 
@@ -61,8 +65,27 @@ class PersonDaoImplTest {
     @DisplayName("Check data in DAO (fixture)")
     @Order(1)
     @Test
-    void dataTest(){
+    void convertJsonRootDataToJavaTest(){
+        //GIVEN
+        String personsJson = Jackson.convertJavaToJson(wrapperList);
+        this.iPersonDAO.getRootFile().setBytes(personsJson.getBytes());
 
+        //WHEN
+        List<Person> personList = Jackson.convertJsonRootDataToJava(
+                this.iPersonDAO.getRootFile().getBytes(),
+                "persons",
+                Person.class);
+        //THEN
+        assertNotNull(personList);
+        assertEquals(personsGiven.size(), personList.size());
+        assertEquals(personsGiven.get(0), personList.get(0));
+        assertEquals(personsGiven.get(1), personList.get(1));
+
+        iPersonDAO.setPersonList(Jackson.convertJsonRootDataToJava(
+                this.iPersonDAO.getRootFile().getBytes(),
+                "persons",
+                Person.class)
+        );
     }
 
     @Order(2)
