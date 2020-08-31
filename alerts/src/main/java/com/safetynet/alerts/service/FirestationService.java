15e0a22cec57a37cfa7e724d968b2356a77b5330
jedi10 <kyle10_dev@hotmail.fr>
2020-08-31 19:39:04 +0200

    Add FirestationService; Unit Test

---
+++ alerts/src/main/java/com/safetynet/alerts/service/FirestationService.java
@@ -0,0 +1,68 @@
+package com.safetynet.alerts.service;
+
+import com.safetynet.alerts.dao.IFirestationDAO;
+import com.safetynet.alerts.dao.IMedicalRecordDAO;
+import com.safetynet.alerts.dao.IPersonDAO;
+import com.safetynet.alerts.models.Firestation;
+import com.safetynet.alerts.models.MedicalRecord;
+import com.safetynet.alerts.models.Person;
+import com.safetynet.alerts.service.rto_models.FirestationRTO;
+import com.safetynet.alerts.service.rto_models.IPersonInfoRTO;
+import com.safetynet.alerts.service.rto_models.PersonInfoRTO;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class FirestationService {
+
+    final
+    IPersonDAO personDAO;
+
+    final
+    IMedicalRecordDAO medicalRecordDAO;
+
+    final
+    IFirestationDAO firestationDAO;
+
+    public FirestationService(IPersonDAO personDAO, IMedicalRecordDAO medicalRecordDAO,
+                              IFirestationDAO firestationDAO) {
+        this.personDAO = personDAO;
+        this.medicalRecordDAO = medicalRecordDAO;
+        this.firestationDAO = firestationDAO;
+    }
+
+    public FirestationRTO getFirestationArea(@NonNull String firestation){
+        FirestationRTO firestationRTO = null;
+        //Get Address List linked with firestation number
+        List<Firestation> firestationList = firestationDAO.findAll();
+        List<String> firestationAddressListResult = firestationList.stream()
+                .filter(e-> e.getStation().equalsIgnoreCase(firestation))
+                .map(Firestation::getAddress)
+                .collect(Collectors.toList());
+
+        if (!firestationAddressListResult.isEmpty()) {
+            //Get Persons List having all address finded
+            List<Person> personList = personDAO.findAll();
+            List<MedicalRecord> medicalRecordList = medicalRecordDAO.findAll();
+            List<IPersonInfoRTO> personInfoRTOListFull =
+                    PersonInfoRTO.buildPersonInfoRTOList(personList, medicalRecordList);
+            List<IPersonInfoRTO> personInfoRTOListFiltered = personInfoRTOListFull.stream()
+                    .filter(e-> firestationAddressListResult.contains(e.getAddress()))
+                    .collect(Collectors.toList());
+            if (!personInfoRTOListFiltered.isEmpty()){
+                //Results are treated to be exposed in a view
+               firestationRTO = new FirestationRTO(personInfoRTOListFiltered);
+            }
+        }
+        return firestationRTO;
+    }
+}
+
+
+//https://www.baeldung.com/java-groupingby-collector
+//https://www.concretepage.com/java/java-8/java-stream-count
